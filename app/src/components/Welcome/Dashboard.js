1e3648d6edfa2f5e77270c663629a4159203c0f4
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-26 13:20:13 -0800

    finishes styling button

--- app/src/components/Welcome/Dashboard.js
+++ app/src/components/Welcome/Dashboard.js
@@ -7,8 +7,14 @@ export default function Dashboard() {
     <div className={classes.main}>
       <Header />
       <h5>Welcome</h5>
-      <p>This onbarding guide will show you how to add your avalibility, and customize your meeting types.</p>
-      <button>let's get started</button>
+      <div className={classes.placeholder}>image placeholder</div>
+      <p>
+        This onbarding guide will show you how to add your avalibility, and
+        customize your meeting types.
+      </p>
+      <div className={classes.buttonContainer}>
+        <button>let's get started</button>
+      </div>
     </div>
   );
 }
