30c923981522b120ac43c81f20dca1dfda1af9ad
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-29 17:35:06 -0800

    finishes styles of CS-5

--- app/src/components/ScheduleAvailableDays/ScheduleAvailableDays.js
+++ app/src/components/ScheduleAvailableDays/ScheduleAvailableDays.js
@@ -1,18 +1,19 @@
 import React, { useReducer, useEffect, useState } from 'react';
-import classes from './ScheduleAvailableDays.module.scss';
-import WeekDays from './WeekDays';
 import { logo, daysOfTheWeek } from '../../store/index';
 import { reducer } from './helpers/reducer';
-
+import classes from './ScheduleAvailableDays.module.scss';
+import WeekDays from './WeekDays';
 import Button from '@material-ui/core/Button';
 const initialState = { daysOfTheWeek };
 
 export default function ScheduleAvailableDays() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [isEverySlected, setIsEverySelected] = useState(false);
 
   useEffect(() => {
     setButtonDisabled(!state.daysOfTheWeek.some((day) => day.selected));
+    setIsEverySelected(!state.daysOfTheWeek.every((day) => day.selected));
   }, [state.daysOfTheWeek]);
 
   return (
@@ -22,25 +23,23 @@ export default function ScheduleAvailableDays() {
           <img src={logo.default} className='logo' alt='logo' />
           <h4>Schedule Availability</h4>
         </div>
-        <div className='body-text'>
+        
           <p>What days are you typically available?</p>
           <p>Select from below.</p>
           <Button
             onClick={() =>
-              dispatch({ type: 'TOGGLE_ALL', payload: !buttonDisabled })
+              dispatch({ type: 'TOGGLE_ALL', payload: isEverySlected })
             }
           >
             {' '}
             Select / Unselect All
           </Button>
-        </div>
+        
       </div>
       <WeekDays state={state} dispatch={dispatch} />
       <div className={classes.buttonContainer}>
-        <Button size='large' disabled={buttonDisabled}>
-          cancel
-        </Button>
-        <Button size='large' color='primary'>
+        <Button size='large'>cancel</Button>
+        <Button size='large' color='primary' disabled={buttonDisabled}>
           save & next
         </Button>
       </div>
--- app/src/components/ScheduleAvailableDays/helpers/reducer.js
+++ app/src/components/ScheduleAvailableDays/helpers/reducer.js
@@ -8,7 +8,7 @@ function reducer(state, action) {
         ),
       };
     case 'TOGGLE_ALL':
-      console.log(action.payload)
+      // console.log(action.payload)
       return {
         ...state,
         daysOfTheWeek: state.daysOfTheWeek.map((day) => ({
