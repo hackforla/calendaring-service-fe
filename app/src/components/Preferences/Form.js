e037e0acaa1315a13fcccbd841b3e76cd8d18d8b
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-04 21:44:48 -0800

    meeting preferences is semi-styled and kind of working but lots of errors in console

--- app/src/components/Preferences/Form.js
+++ app/src/components/Preferences/Form.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
@@ -12,6 +15,7 @@ const BootstrapInput = withStyles((theme) => ({
   root: {
     'label + &': {
       marginTop: theme.spacing(3),
+      border: '1px solid #595959',
     },
   },
   input: {
@@ -22,119 +26,125 @@ const BootstrapInput = withStyles((theme) => ({
     fontSize: 16,
     padding: '10px 26px 10px 12px',
     transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '1px solid #595959',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
   },
 }))(InputBase);
 
-const useStyles = makeStyles((theme) => ({
-  margin: {},
-}));
-
 export default function Form() {
-  const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [myState, setMyState] = useState({});
   const handleChange = (event) => {
-    setAge(event.target.value);
+    let myNewState = myState;
+    myNewState[event.target.name] = event.target.value;
+    console.log(myNewState);
+    setMyState(myNewState);
   };
+
+  console.log(myState);
+
   return (
     <div style={{ padding: '1rem' }}>
-      <FormControl className={classes.margin} fullWidth={true}>
-        <FormHelperText>
-          {/* <InputLabel htmlFor='demo-customized-select-native' required={true}> */}
-          Meeting Name
-          {/* </InputLabel> */}
-        </FormHelperText>
-
+      <FormControl fullWidth={true}>
+        <FormHelperText>Meeting name</FormHelperText>
         <Select
-          id='demo-customized-select-native'
-          value={age}
+          labelId=''
+          id='demo-simple-select'
           onChange={handleChange}
-          autoWidth={true}
+          value={myState.meetingName || ''}
           input={<BootstrapInput />}
-          required={true}
+          
+          name='meetingName'
         >
-          <MenuItem aria-label='None' value='' />
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
-      <FormControl className={classes.margin} fullWidth={true}>
+      <FormControl fullWidth={true}>
         <FormHelperText>Location</FormHelperText>
         <Select
           labelId=''
           id='demo-simple-select'
           onChange={handleChange}
-          value={''}
+          value={myState.location || ''}
           input={<BootstrapInput />}
           required={true}
+          name='location'
         >
-          <MenuItem value=''>please select a value</MenuItem>
-          <MenuItem>In person meeting</MenuItem>
-          <MenuItem>Web conference</MenuItem>
-          <MenuItem>Phone call</MenuItem>
+          <MenuItem value='in person meeting'>In person meeting</MenuItem>
+          <MenuItem value='web conference'>Web conference</MenuItem>
+          <MenuItem value='phone call'>Phone call</MenuItem>
         </Select>
       </FormControl>
-      <FormControl className={classes.margin} fullWidth={true}>
-        <FormHelperText>Duration</FormHelperText>
-        <Select
-          labelId=''
-          id='demo-simple-select'
-          onChange={handleChange}
-          value={''}
-          input={<BootstrapInput />}
-          required={true}
-        >
-          <MenuItem value=''>please select a value</MenuItem>
-          <MenuItem>15 min</MenuItem>
-          <MenuItem>30 min</MenuItem>
-          <MenuItem>45 min</MenuItem>
-          <MenuItem>60 min</MenuItem>
-          <MenuItem>90 min</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.margin} fullWidth={true}>
-        <FormHelperText>Meeting Color</FormHelperText>
-        <Select
-          labelId=''
-          id='demo-simple-select'
-          onChange={handleChange}
-          value={''}
-          input={<BootstrapInput />}
-          required={true}
-        >
-          <MenuItem value=''>please select a value</MenuItem>
-          <MenuItem>hi</MenuItem>
-          <MenuItem>Pink</MenuItem>
-          <MenuItem>Purple</MenuItem>
-          <MenuItem>Blue</MenuItem>
-          <MenuItem>Green</MenuItem>
-        </Select>
-      </FormControl>
+
+      <FormHelperText>Is this meeting location on-site?</FormHelperText>
+      <FormGroup row>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={myState.checkedA || false}
+              onChange={handleChange}
+              name='checkedA'
+              color='primary'
+            />
+          }
+          label='Yes'
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={myState.checkedB || false}
+              onChange={handleChange}
+              name='checkedB'
+              color='primary'
+            />
+          }
+          label='No'
+        />
+      </FormGroup>
+      <FormGroup row>
+        <FormControl fullWidth={true}>
+          <FormHelperText>Duration</FormHelperText>
+          <Select
+            labelId=''
+            id='demo-simple-select'
+            onChange={handleChange}
+            value={myState.duration || ''}
+            input={<BootstrapInput />}
+            required={true}
+            name='duration'
+          >
+            <MenuItem value={15}>15 min</MenuItem>
+            <MenuItem value={30}>30 min</MenuItem>
+            <MenuItem value={45}>45 min</MenuItem>
+            <MenuItem value={60}>60 min</MenuItem>
+            <MenuItem value={90}>90 min</MenuItem>
+          </Select>
+
+          <FormHelperText>Meeting Color</FormHelperText>
+          <Select
+            labelId=''
+            id='demo-simple-select'
+            onChange={handleChange}
+            value={myState.color || ''}
+            input={<BootstrapInput />}
+            required={true}
+            name='color'
+          >
+            <MenuItem value=''>please select a value</MenuItem>
+            <MenuItem value='red'>Red</MenuItem>
+            <MenuItem value='pink'>Pink</MenuItem>
+            <MenuItem value='purple'>Purple</MenuItem>
+            <MenuItem value='blue'>Blue</MenuItem>
+            <MenuItem value='green'>Green</MenuItem>
+          </Select>
+        </FormControl>
+      </FormGroup>
       <FormControl fullWidth={true}>
         <FormHelperText>Description</FormHelperText>
-
         <TextField
           id='outlined-basic'
-          className={classes.margin}
           variant='outlined'
+          name='description'
+          onChange={handleChange}
         />
       </FormControl>
 
--- app/src/store/constants.js
+++ app/src/store/constants.js
@@ -9,11 +9,37 @@ const daysOfTheWeek = [
 ];
 
 const meetingTypes = [
-  {type: 'Training', updateNeeded: false},
-  {type: 'Interview', updateNeeded: true},
-  {type: 'Home Inspection', updateNeeded: true},
-  {type: 'Home Selection', updateNeeded: false},
-  {type: 'Home Visit', updateNeeded: true},
+  { type: 'Training', updateNeeded: false },
+  { type: 'Interview', updateNeeded: true },
+  { type: 'Home Inspection', updateNeeded: true },
+  { type: 'Home Selection', updateNeeded: false },
+  { type: 'Home Visit', updateNeeded: true },
+];
+
+const formFields = [
+  {
+    name: 'MeetingName',
+    inputs: ['ten', 'twenty', 'thirty'],
+  },
+  {
+    name: 'Location',
+    inputs: ['In person meeting', 'Web conference', 'Phone call'],
+  },
+  {
+    name: 'Is this meeting location on-site?',
+    inputs: ['Yes', 'No'],
+  },
+  {
+    name: 'Duration',
+    inputs: [15, 30, 45, 60, 90],
+  },
+  {
+    name: 'Meeting Color',
+    inputs: ['Red', 'Pink', 'Purple', 'Blue', 'Green'],
+  },
+  {
+    name: 'Description',
+  },
 ];
 
 export { daysOfTheWeek, meetingTypes };
