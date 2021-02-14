150512f9a7b89d74869708695650237fb220a7aa
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-02-13 22:54:39 -0800

    reorganizes material-components file

--- app/src/store/material-components.js
+++ app/src/store/material-components.js
@@ -1,20 +1,21 @@
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import Select from '@material-ui/core/Select';
-import { TextareaAutosize } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import InputBase from '@material-ui/core/InputBase';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Checkbox from '@material-ui/core/Checkbox';
+import {
+  Select,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  ListItemIcon,
+  FormGroup,
+  FormControl,
+  MenuItem,
+  Button,
+  FormHelperText,
+  TextareaAutosize,
+  InputBase,
+  Card,
+  CardContent,
+  Checkbox
+} from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 export {
   Button,
