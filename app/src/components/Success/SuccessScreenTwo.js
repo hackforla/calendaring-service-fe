62d9c8e3c70162c3c144a4570c0542b452b475de
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-03-21 19:52:37 -0700

    removes unneeded styles from successscreentwo

--- app/src/components/Success/SuccessScreenTwo.js
+++ app/src/components/Success/SuccessScreenTwo.js
@@ -4,23 +4,27 @@ import { Button } from '../../store/index';
 import { WhiteCheckmarkIcon, Typography } from '../../store/index';
 
 export default function SuccessScreenTwo() {
-    return (
-        <div className={classes.main}>
-            <img src={WhiteCheckmarkIcon.default} className='whiteCheckmarkIcon' alt='whiteCheckMarkIcon' />
-            <div className={classes.SuccessScreenTwoHeading}>
-            <Typography variant='h5'>Success!</Typography>
-            </div>
-            <div className={classes.SuccessScreenTwoText}>
-                <p className='body-text'>
-                    Your meeting preferences for TRAINING have been set. Please continue set up and manage your preferences by visisting your dashboard.
-                </p>
-            </div>
-            <div className={classes.SuccessScreenTwoButton}>
-                <Button variant='contained' color='primary'>
-                    Complete Setup
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.main}>
+      <img
+        src={WhiteCheckmarkIcon.default}
+        className='whiteCheckmarkIcon'
+        alt='whiteCheckMarkIcon'
+      />
+      <div className={classes.SuccessScreenTwoHeading}>
+        <Typography variant='h5'>Success!</Typography>
+      </div>
+      <div className={classes.SuccessScreenTwoText}>
+        <Typography className='body-text'>
+          Your meeting preferences for TRAINING have been set. Please continue
+          set up and manage your preferences by visisting your dashboard.
+        </Typography>
+      </div>
+      <div className={classes.SuccessScreenTwoButton}>
+        <Button variant='contained' color='primary'>
+          Complete Setup
+        </Button>
+      </div>
+    </div>
+  );
 }
-
