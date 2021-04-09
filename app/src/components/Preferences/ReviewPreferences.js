ffdfeba9f3fa849ab6e0b1bf7df82765b5e4dc70
Paul Choi <paulsoonminchoi@gmail.com>
2021-04-08 17:12:09 -0700

    positions buttons on bottom right corner

--- app/src/components/Preferences/ReviewPreferences.js
+++ app/src/components/Preferences/ReviewPreferences.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../shared/Header';
 import PreferencesCard from './PreferencesCard';
+import { Button } from '../../store/index';
 
 export default function ReviewPreferences() {
   return (
@@ -10,6 +11,17 @@ export default function ReviewPreferences() {
         subtitle='Review the information is correct before creating'
       />
       <PreferencesCard />
+      <div
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+        }}>
+        <Button size='large'>cancel</Button>
+        <Button size='large' color='primary'>
+          save & next
+        </Button>
+      </div>
     </>
   );
 }
