2203667e7fc48b685f7e0864ba5eda5532af9f09
Alvin Delito <alvindelito@live.com>
2021-05-01 11:37:44 -0700

    complete ContactReview state and styles

--- app/src/components/Scheduling/ContactReview.js
+++ app/src/components/Scheduling/ContactReview.js
@@ -1,10 +1,129 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import {
+  FormGroup,
+  TextField,
+  FormControl,
+  Button,
+} from '../../store/index';
+import { useStyles } from './ContactReviewStyles';
+
+const HARD_CODED_DATA = {
+  caseWorker: 'Tracy',
+  firstName: 'Nina',
+  lastName: 'Mosley',
+  email: 'nina99@gmail.com',
+  phoneNumber: '12135555555'
+}
 
 export default function ContactReview() {
+  const classes = useStyles();
+  const [inputs, setInputs] = useState({
+    caseWorker: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  })
+
+  const [editMode, toggleEdit] = useState({
+    email: false,
+    phoneNumber: false,
+  });
+
+  useEffect(() => {
+    setInputs(HARD_CODED_DATA);
+  }, [])
+
+  const onChange = (e) => {
+    setInputs({...inputs, [e.target.name]: e.target.value})
+  };
+
   return (
-    <>
-      <Header />
-    </>
+    <div className={classes.root}>
+      <Header
+        title="Contact Information"
+        description="Review your profile information below"
+        activity={`Training with ${HARD_CODED_DATA.caseWorker}`}
+      />
+
+      <FormControl>
+        <label>First Name <span className={classes.required}>*</span></label>
+        <TextField
+          variant="outlined"
+          fullWidth name="firstName"
+          value={inputs.firstName}
+          inputProps={{
+              readOnly: true,
+              "aria-readonly": true,
+            }}
+            required
+        />
+        <label>Last Name <span className={classes.required}>*</span></label>
+        <TextField
+          variant="outlined"
+          fullWidth name="lastName"
+          value={inputs.lastName}
+          inputProps={{
+            readOnly: true,
+            "aria-readonly": true,
+          }}
+          required
+          />
+        <>
+          <label>Email Address <span className={classes.required}>*</span></label>
+          <TextField
+            type="email"
+            variant="outlined"
+            fullWidth name="email"
+            value={inputs.email}
+            onChange={onChange}
+            disabled={!editMode.email}
+            InputProps={{
+              classes: {
+                disabled: classes.disabled,
+                input: classes.inputWidth
+              },
+            }}
+            required
+          />
+          <Button
+            color="primary"
+            className={classes.editButton}
+            onClick={() => toggleEdit({ ...editMode, email: !editMode.email})}
+          >
+            {editMode.email ? 'confirm' : 'edit' }
+          </Button>
+        </>
+        <>
+        <label>Phone Number (Optional)</label>
+        <TextField
+          type="text"
+          variant="outlined"
+          fullWidth name="phoneNumber"
+          value={inputs.phoneNumber}
+          onChange={onChange}
+          disabled={!editMode.phoneNumber}
+          InputProps={{
+            classes: {
+              disabled: classes.disabled,
+              input: classes.inputWidth
+            },
+          }}
+        />
+        <Button
+          color="primary"
+          className={classes.editButton}
+          onClick={() => toggleEdit({ ...editMode, phoneNumber: !editMode.phoneNumber})}
+        >
+          {editMode.phoneNumber ? 'confirm' : 'edit' }
+        </Button>
+        </>
+      </FormControl>
+      <div className={classes.buttonContainer}>
+        <Button size='large'>Go Back</Button>
+        <Button size='large' color='primary'>Next</Button>
+      </div>
+    </div>
   )
 }
--- app/src/components/Scheduling/ContactReviewStyles.js
+++ app/src/components/Scheduling/ContactReviewStyles.js
@@ -5,8 +5,34 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
-  }
+    margin: '1rem',
+  },
+  editButton: {
+    width: '37px',
+    height: '22px',
+    position: 'relative',
+    marginLeft: 'auto',
+    marginRight: '0',
+    bottom: '40px',
+    right: '20px',
+    textTransform: 'capitalize',
+    letterSpacing: '1px',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    margin: '1rem',
+  },
+  inputWidth: {
+    width: '65%',
+  },
+  required: {
+    color: 'red',
+  },
+  disabled: {
+    color: '#2F2F2F',
+  },
 })
 
 export { useStyles };
