b2ef467c2d44a93d4d62df9ea478d0594585de67
Paul Choi <paulsoonminchoi@gmail.com>
2021-05-17 11:29:03 -0700

    Updates Header component to use action prop instead of children prop to reduce confusion and default timezone text

--- app/src/components/Scheduling/Header.js
+++ app/src/components/Scheduling/Header.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { logo, Typography } from '../../store/index';
+import { logo, Typography, ArrowDropDownIcon } from '../../store/index';
 import { useStyles } from './CalendarViewStyles';
 
-export default function Header({ title, description, activity, children }) {
+export default function Header({
+  title,
+  description,
+  activity,
+  action,
+  slots,
+}) {
   const classes = useStyles();
+  const slotsDoubled = slots * 2;
+
+  const actionText = () => {
+    if (action && slots) {
+      return (
+        <Typography
+          variant='body2'
+          style={{
+            fontSize: '14px',
+            marginBottom: '1rem',
+          }}>
+          {`${action} (${slotsDoubled} total)`}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography
+          variant='body2'
+          style={{
+            fontSize: '14px',
+            marginBottom: '1rem',
+          }}>
+          {action}
+        </Typography>
+      );
+    }
+  };
 
   return (
     <div className={classes.root}>
       <div className={classes.header}>
         <img src={logo.default} className='logo' alt='logo' />
-        <Typography variant='h4'>{title}</Typography>
+        <Typography
+          variant='h4'
+          style={{
+            fontSize: '24px',
+          }}>
+          {title}
+        </Typography>
       </div>
       <Typography
         variant='subtitle1'
@@ -18,7 +57,7 @@ export default function Header({ title, description, activity, children }) {
           width: '200px',
           textAlign: 'center',
         }}>
-          {description}
+        {description}
       </Typography>
       <Typography
         variant='h6'
@@ -28,6 +67,19 @@ export default function Header({ title, description, activity, children }) {
         color='primary'>
         {activity}
       </Typography>
+
+      {actionText()}
+
+      <div className={classes.headerTimeZone}>
+        <Typography
+          variant='body2'
+          style={{
+            fontWeight: '200',
+          }}>
+          Pacific Standard Time (US and Canada)
+        </Typography>
+        <ArrowDropDownIcon />
+      </div>
     </div>
   );
 }
