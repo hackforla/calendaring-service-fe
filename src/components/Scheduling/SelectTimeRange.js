c99463bd31ad50d7ed9b26503f14c93fa9ad3613
Paul Choi <paulsoonminchoi@gmail.com>
2021-05-22 11:42:13 -0700

    Added comments to be reviewed for SelectTimeRange functionality issue

--- src/components/Scheduling/SelectTimeRange.js
+++ src/components/Scheduling/SelectTimeRange.js
@@ -4,11 +4,11 @@ import {
   FormControl,
   Select,
   InputLabel,
-  // MenuItem,
+  MenuItem,
   KeyboardArrowDownIcon,
 } from '../../utils/index';
 import { useStyles } from './SelectTimeRangeStyles';
-import MuiMenuItem from './MuiMenuItem';
+// import MuiMenuItem from './MuiMenuItem';
 
 export default function SelectTimeRange({
   day,
@@ -20,6 +20,7 @@ export default function SelectTimeRange({
   const classes = useStyles();
   const [times, setTimes] = useState([]);
 
+  //Validation check to handle if user selects more than 2 times.
   const handleChange = (event) => {
     if (event.target.value.length <= 2) {
       setTimes(event.target.value);
@@ -93,20 +94,30 @@ export default function SelectTimeRange({
         >
           {availableTimes.map((time, index) => {
             return (
-              <MuiMenuItem
-                key={index}
-                time={time}
-                handleChange={handleChange}
-              />
-              // <MenuItem
-              //   classes={{
-              //     root: classes.menuItemRoot,
-              //   }}
+              // The MuiMenuItem allows me to have its own state which gives me the ability to show the Checkmark.
+
+              // But for some reason by doing this approach, the onChange on the Select Material UI won't trigger and therefore, I would not get my times date stored.
+
+              //I then tried to pass the handleChange to the my MuiMenuItem component but it seems that the event.target.value is not the same at when the select's event.target.value come out to be. Select's event.target.value is an ['1pm-2pm', '2pm-3pm'], while the MuiMenuItem's event.target.value comes out to be a numeric value.
+
+              //You can uncomment MuiMenuItem and the following lines needed and comment out MenuItem to see the checkmark functionality in action.
+
+              // <MuiMenuItem
               //   key={index}
-              //   value={time}
-              // >
-              //   {time}
-              // </MenuItem>
+              //   time={time}
+              //   handleChange={handleChange}
+              // />
+
+              //This MenuItem allows me to select times and get the times stored into the times state.
+              <MenuItem
+                classes={{
+                  root: classes.menuItemRoot,
+                }}
+                key={index}
+                value={time}
+              >
+                {time}
+              </MenuItem>
             );
           })}
         </Select>
