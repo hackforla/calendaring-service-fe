40c1f2f1e482f24daf5ec5e2db6aab122610159e
Jonathan Butler <jonathan.butler@getweave.com>
2021-05-31 19:17:50 -0700

    adds object shape

--- src/utils/redux/userSlice.js
+++ src/utils/redux/userSlice.js
@@ -1,14 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+/**
+ * pre-requisite -- you will need the local java service running on port 8080
+Request URL: http://localhost:8080/meetingTypes/create <-- this is what you call when you click "Submit" for when the case worker is finished with created the meeting types/preferences
+Json payload (shape):
+{
+    "name":"Training" | "Inspection" | "The other meeting type" | "Move In" | "The last meeting type"
+    "location":"enter location here",
+    "duration":"30" | "60"
+    "color":"enter color here",
+    "description":"enter description here",
+    "isOnSite":"0" | "1"
+} 
+ */
+const initialState = {
+  user: {
+    name: '',
+    verified: false,
+  },
+  meetingPreferences: {},
+};
+
 export const userSlice = createSlice({
   name: 'user',
-  initialState: {
-    user: {
-      name: '',
-      verified: false,
-    },
-    meetingPreferences: {},
-  },
+  initialState,
   reducers: {
     setName: (state, action) => {
       state.user.name = action.payload;
