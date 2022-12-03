f650b4c0b3acdabb218b388307103ea8a276e3dc
Erik <eguntner2291@gmail.com>
2022-12-02 16:20:21 -0800

    Test out updated stayling and mui components

--- src/components/ScheduleAvailability/Header.tsx
+++ src/components/ScheduleAvailability/Header.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
-import { Typography } from '../../utils';
+import { Typography, Stack } from '@mui/material';
 import logo from '../../utils/images/Logo.png';
 
 export default function Header() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div
-        style={{
-          display: 'flex',
+    <Stack sx={{ alignItems: 'center' }}>
+      <Stack
+        sx={{
           alignItems: 'center',
           justifyContent: 'center',
           flexDirection: 'row',
+          marginTop: 2,
+          marginBottom: 1,
         }}
       >
         <img src={logo} className='logo' alt='logo' />
-        <Typography variant='h4' style={{ margin: '2rem 0' }}>
-          Schedule Availability
-        </Typography>
-      </div>
-      <Typography
-        variant='body1'
-        style={{ margin: '0 auto', paddingLeft: '3rem' }}
-      >
+        <Typography variant='h4'>Schedule Availability</Typography>
+      </Stack>
+      <Typography variant='body1'>
         What days are you typically available?
       </Typography>
-      <Typography
-        variant='body1'
-        style={{ margin: '0 auto', paddingLeft: '3rem' }}
-      >
+      <Typography sx={{ marginBottom: 1 }} variant='body1'>
         Select from below.
       </Typography>
-    </div>
+    </Stack>
   );
 }
